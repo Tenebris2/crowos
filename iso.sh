5e1b56a9a27089c9c5c563467b715fc442e22cee
Bui Duc Anh <buiducanh567@gmail.com>
2026-05-02 14:55:44 +0700

    refactor: change os name

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/crowos.kernel isodir/boot/crowos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "crowos" {
+	multiboot /boot/crowos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o crowos.iso isodir
